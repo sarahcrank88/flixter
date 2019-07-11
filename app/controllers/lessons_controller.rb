bafb4fd774397e1b8600b9f8a3d35f840beff1bd
Sarah Crank <sarahcrank88@gmail.com>
2019-07-11 00:37:23 +0000

    Lesson 28 pt 1

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,8 +1,17 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_lesson, only: [:show]
+  
   def show
   end
   
   private
+  
+  def require_authorized_for_current_lesson
+    if current_user.enrolled_in? != current_user
+      redirect_to course_path(current_course), alert: 'You must be enrolled in course to view course content'
+    end
+  end
 
   helper_method :current_lesson
   def current_lesson
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,12 @@ class User < ApplicationRecord
   has_many :enrolled_courses, through: :enrollments, source: :course
 
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-         
+         :recoverable, :rememberable, :trackable, :validatable
+
   def enrolled_in?(course)
     return enrolled_courses.include?(course)
   end
-  
+
 end
