04262d2f44ca831c3f4fa84ebc44d239baa83404
Sarah Crank <sarahcrank88@gmail.com>
2019-07-18 00:09:33 +0000

    Lesson 28: Permissions - Finally complete

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -8,8 +8,8 @@ class LessonsController < ApplicationController
   private
   
   def require_authorized_for_current_lesson
-    if current_lesson.section.course.user.enrolled_in?(current_lesson) != current_user
-      redirect_to course_path, alert: 'You must be enrolled in course to view course content', status: :unauthorized
+    unless current_user.enrolled_in?(current_lesson.section.course)
+      redirect_to current_lesson.section.course, alert: 'You must be enrolled in course to view course content'
     end
   end
 
