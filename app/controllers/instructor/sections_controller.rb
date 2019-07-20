9917e6549c126771fedfb0d4b9a32717d7c81b11
Sarah Crank <sarahcrank88@gmail.com>
2019-07-20 03:16:54 +0000

    Lesson 7-36: New lesson form modal

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -7,37 +7,34 @@ class Instructor::SectionsController < ApplicationController
     @section = current_course.sections.create(section_params)
     redirect_to instructor_course_path(current_course)
   end
-  
+
   def update
     current_section.update_attributes(section_params)
-    render plain: 'updated!'
+    render plain: 'ok!'
   end
 
   private
-  
+
   def require_authorized_for_current_section
     if current_section.course.user != current_user
-      render plain: 'Unauthorized', status: :unauthorized
+      render plain: "Unauthorized", status: :unauthorized
     end
   end
-  
+
   def current_section
     @current_section ||= Section.find(params[:id])
   end
+
 
   def require_authorized_for_current_course
     if current_course.user != current_user
       render plain: "Unauthorized", status: :unauthorized
     end
   end
-  
+
   helper_method :current_course
   def current_course
-    if params[:course_id]
-      @current_course ||= Course.find(params[:course_id])
-    else
-      current_section.course
-    end
+    @current_course ||= Course.find(params[:course_id])
   end
 
   def section_params
